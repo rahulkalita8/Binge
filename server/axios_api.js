285105fb4725a03eb83fdf89aae0f6846b1dee73
lgdeloss <lgdeloss@ncsu.edu>
2021-09-21 19:31:39 -0400

    used OMDB api, made changes to method call

--- server/axios_api.js
+++ server/axios_api.js
@@ -1,61 +1,31 @@
-var axios = require("axios").default;
-var HOST = "movies-tvshows-data-imdb.p.rapidapi.com";
-var API_KEY = "030611f594msh73349b7b00af6e8p16d57ejsncbf1b45a6165";
-var URL = "https://movies-tvshows-data-imdb.p.rapidapi.com/";
+const { default: axios } = require("axios");
+const RATINGS = {
+    RottenTomatoes: "",
+    IMDB: ""
+}
 
+const getDetails = async(title) => {
+    var API_KEY="631772a5";
+    var URL=`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
 
+    try {
+         const response = await axios.get(URL)
+         const ratings = response.data['Ratings'];
+         ratings.forEach(element => {
+             if (element['Source']=="Internet Movie Database"){
+                RATINGS["RottenTomatoes"]=element["Value"];
+             }
+             if (element['Source']=="Rotten Tomatoes"){
+                 RATINGS["IMDB"] = element["Value"];
+            }
+         });
+         } catch(errors){
+             console.error(errors)
+         }
 
-function get_details(id) {
-  var options = {
-    method: 'GET',
-    url: URL,
-    params: {type: 'get-movie-details', imdb: id},
-    headers: {
-      'x-rapidapi-host': HOST,
-      'x-rapidapi-key': API_KEY
+    console.log(RATINGS)
+    return RATINGS;
     }
-  };
-  axios.request(options).then(function (response) {
-    console.log("rating:"+response.data["imdb_rating"]+"\n"+"popularity:"+response.data["popularity"]+"\n"+"vote:"+response.data["vote_count"]);
-  }).catch(function (error) {
-    console.error(error);
-  });
-
-}
-
-
-function get_id(t){
-  var options = {
-    method: 'GET',
-    url: URL,
-    params: {type: 'get-movies-by-title', title: t},
-    headers: {
-      'x-rapidapi-host': HOST,
-      'x-rapidapi-key': API_KEY
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-    var results = response.data.movie_results;
-    results.forEach(function(e,i){
-      console.log(e['imdb_id'])
-      return(e['imdb_id'])
-    });
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-
-
-// Local test
-
-async function main(){
-  var test_query = "i am legend";
-  var id = await get_id(test_query);
-  get_details(id);
-}
-main();
 
 
 
