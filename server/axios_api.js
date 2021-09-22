a31193bd721bf751750c43f8c8e5543f1c676aa1
Vignesh Muthukumar <vickymhs@gmail.com>
2021-09-22 19:11:19 -0400

    Added css and fetching movie title in cast screen

--- server/axios_api.js
+++ server/axios_api.js
@@ -1,28 +1,54 @@
 const { default: axios } = require("axios");
-var API_KEY="631772a5";
+var API_KEY = "631772a5";
 
 
-const getRatings = async(title) => {
-    var URL=`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
+const getRatings = async (title) => {
+    var URL = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
     const ratings = {
         RottenTomatoes: "",
         IMDB: ""
     };
     try {
-         const response = await axios.get(URL)
-         const data = response.data.Ratings;
-         data.forEach(rating => {
-             if (rating.Source=="Internet Movie Database"){
-                ratings.IMDB=rating.Value;
-             }
-             if (rating.Source=="Rotten Tomatoes"){
-                 ratings.RottenTomatoes = rating.Value;
+        const response = await axios.get(URL)
+        const data = response.data.Ratings;
+        data.forEach(rating => {
+            if (rating.Source == "Internet Movie Database") {
+                ratings.IMDB = rating.Value;
             }
-         });
-         } catch(errors){
-             console.error(errors);
-         }
-    return ratings;
+            if (rating.Source == "Rotten Tomatoes") {
+                ratings.RottenTomatoes = rating.Value;
+            }
+        });
+    } catch (errors) {
+        console.error(errors);
     }
+    return ratings;
+}
 
+
+const getMovieDetails = async (title) => {
+    var URL = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
+    const movieDetails = {
+        RottenTomatoes: "",
+        IMDB: "",
+        Cast: "",
+    };
+    try {
+        const response = await axios.get(URL)
+        const data = response.data;
+        const ratings = data.Ratings;
+        const cast = data.Actors;
+        ratings.forEach(rating => {
+            if (rating.Source == "Internet Movie Database") {
+                movieDetails.IMDB = rating.Value;
+            }
+            if (rating.Source == "Rotten Tomatoes") {
+                movieDetails.RottenTomatoes = rating.Value;
+            }
+        });
+    } catch (errors) {
+        console.error(errors);
+    }
+    return movieDetails;
+}
 
