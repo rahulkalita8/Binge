225467e99e818e8789e278ff8b912f4429cb5f09
Eshita Arza <Eshita@MacBook-Pro.lan>
2021-09-22 23:49:14 -0400

    Added basic div for ratings in modal screen
    added comments to function

--- server/axios_api.js
+++ server/axios_api.js
@@ -1,13 +1,22 @@
 const { default: axios } = require("axios");
+// HARD CODED API KEY
 var API_KEY="631772a5";
 
-
 const getRatings = async(title) => {
+    /** 
+     *Funtion to read the title from api call and return ratings from IMBD and RottenTomatoes
+     * @param {title}
+     * @return {ratings}
+    */
+    
+     // GET request 
     var URL=`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
+    //Object that returns with values
     const ratings = {
         RottenTomatoes: "",
         IMDB: ""
     };
+    // Makes request and returns the ratings object populated
     try {
          const response = await axios.get(URL)
          const data = response.data.Ratings;
