21c7e3c5d45d90ffe78d1bb8ce9bc23b7b6c44c2
Vignesh Muthukumar <43530800+vickymhs@users.noreply.github.com>
2021-09-24 18:36:41 -0400

    Cast screen css (#8)
    * Adding cast details in the watch screen
    
    * Added css and fetching movie title in cast screen
    
    * Added cast fetching based on title - incomplete
    
    * Rebased and resolved merge conflicts with main
    
    Co-authored-by: Rahul Kalita <rahulkalita8@gmail.com>

--- client/js/netflix/watchScreen.js
+++ client/js/netflix/watchScreen.js
@@ -1,3 +1,5 @@
+
+const CAST = 'actors';
 function createCastList(casts){
     let castList = document.createElement('ol');
     castList.className = "cast-list"
@@ -25,18 +27,94 @@ function listAlreadyExists(){
 
 function addToWatchScreen(casts){
     if(!listAlreadyExists()){
-        let watchDiv = document.getElementsByClassName('ltr-fntwn3')
-        watchDiv[0].appendChild(createCastDiv(casts))    
+        let castDiv = document.getElementsByClassName('ltr-fntwn3')
+        castDiv[0].appendChild(createCastDiv(casts))    
     }    
 }
 
-casts = ["Salmon Bhoi", "ShehRakh Khon", "Amor Khon"]
+/**
+ * Function to get cast details based on the movie title
+ * @param {*} title 
+ * @returns 
+ */
+const getCastDetails = async (title) => {
+    const castDetailsInLocalStorage = getCastDetailsFromLocalStorage(title);
+
+    if (castDetailsInLocalStorage) {
+        return castDetailsInLocalStorage;
+    }
+
+    const castDetails = await fetchRatings(title);
+    storeCastDetailsInLocalStorage(title, castDetails);
+    return castDetails;
+};
+
+/**
+ * Function to get cast details from local browser storage
+ * @param {*} title 
+ * @returns 
+ */
+function getCastDetailsFromLocalStorage(title) {
+    const castDetailsInLocalStorage = JSON.parse(localStorage.getItem(CAST));
+    const castDetails = castDetailsInLocalStorage && castDetailsInLocalStorage[title];
+    return castDetails;
+}
+
+/**
+ * Function to store cast details in local storage
+ * @param {*} title 
+ * @param {*} castDetails 
+ */
+function storeCastDetailsInLocalStorage(title, castDetails) {
+    if (title && castDetails) {
+        let castDetailsInStorage = JSON.parse(localStorage.getItem(CAST));
+        if (!castDetailsInStorage) {
+            castDetailsInStorage = {};
+        }
+        castDetailsInStorage[title] = castDetails;
+        localStorage.setItem(CAST, JSON.stringify(castDetailsInStorage));
+    }
+}
+
+function getCastDetails1(title){
+    casts = ["Salmon Bhoi", "ShehRakh Khon", "Amor Khon"];
+    return casts;
+}
 
 // addToWatchScreen(casts)
+
+/**
+ * Function to get the title of the movie / series that is currently playing in the screen
+ * @returns 
+ */
+function getTitle() {
+    let title = ''
+    let bottomControlSection = document.getElementsByClassName("ltr-kpws2k").item(0);
+    let bottomControlSectionInfo = bottomControlSection.getElementsByTagName("h4");
+    // console.log(bottomControlSectionInfo)
+    
+    // For movies, info is obtained this way
+    if(bottomControlSectionInfo === null || bottomControlSectionInfo.length === 0) {
+        title = bottomControlSection.innerHTML;
+    }
+    else {
+        // For series, info is obtained this way because it has series title name + episode number aswell
+        title = bottomControlSection.getElementsByTagName("h4").item(0).innerHTML;
+    }
+    console.log("Movie title ", title);
+    return title;
+}
 
 var mutationObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if(mutation.target.className === 'active ltr-fntwn3'){
+            const title = getTitle();
+            
+            // Get the cast details
+            // const movieDetails = getCastDetails(title);
+            const casts = getCastDetails1(title);
+            console.log("printing cast details ", casts);
+            console.log(typeof(casts));
             addToWatchScreen(casts)
         }
         else if(mutation.target.className == 'inactive ltr-fntwn3'){
--- server/axios_api.js
+++ server/axios_api.js
@@ -1,6 +1,5 @@
 const { default: axios } = require("axios");
-// HARD CODED API KEY
-var API_KEY="631772a5";
+var API_KEY = "631772a5";
 
 const getRatings = async(title) => {
     /** 
@@ -33,56 +32,3 @@ const getRatings = async(title) => {
          }
     return ratings;
     }
-var axios = require("axios").default;
-var HOST = "movies-tvshows-data-imdb.p.rapidapi.com";
-var API_KEY = "030611f594msh73349b7b00af6e8p16d57ejsncbf1b45a6165";
-var URL = "https://movies-tvshows-data-imdb.p.rapidapi.com/";
-
-
-
-function get_details(id) {
-  var options = {
-    method: 'GET',
-    url: URL,
-    params: {type: 'get-movie-details', imdb: id},
-    headers: {
-      'x-rapidapi-host': HOST,
-      'x-rapidapi-key': API_KEY
-    }
-  };
-  axios.request(options).then(function (response) {
-    console.log("rating:"+response.data["imdb_rating"]+"\n"+"popularity:"+response.data["popularity"]+"\n"+"vote:"+response.data["vote_count"]);
-  }).catch(function (error) {
-    console.error(error);
-  });
-
-const RATINGS = {
-    RottenTomatoes: "",
-    IMDB: ""
-}
-
-
-const getRatings = async(title) => {
-    var URL=`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
-    const ratings = {
-        RottenTomatoes: "",
-        IMDB: ""
-    };
-    try {
-         const response = await axios.get(URL)
-         const data = response.data.Ratings;
-         data.forEach(rating => {
-             if (rating.Source=="Internet Movie Database"){
-                ratings.IMDB=rating.Value;
-             }
-             if (rating.Source=="Rotten Tomatoes"){
-                 ratings.RottenTomatoes = rating.Value;
-            }
-         });
-         } catch(errors){
-             console.error(errors);
-         }
-    return ratings;
-    }
-
-
